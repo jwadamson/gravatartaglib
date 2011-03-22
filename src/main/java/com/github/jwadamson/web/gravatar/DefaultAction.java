f1dd8dc9e96436eeaaea4c7c4d10c55cb377bd0f
Jeffrey Adamson <jwa@urbancode.com>
2011-03-21 21:31:36 -0400

    some minor refactoring and fixes

---
+++ src/main/java/com/github/jwadamson/web/gravatar/DefaultAction.java
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+package com.github.jwadamson.web.gravatar;
+
+import org.apache.commons.lang.StringUtils;
+
+public enum DefaultAction {
+
+    //*****************************************************************************
+    // CLASS
+    //*****************************************************************************
+
+    IMAGE(""),
+    DO_404("404"),
+    MM,
+    IDENTICON,
+    MONSTERID,
+    WAVATAR,
+    RETRO;
+
+    //*****************************************************************************
+    // INSTANCE
+    //*****************************************************************************
+
+    private final String value;
+
+    /**
+     *
+     */
+    private DefaultAction() {
+        this(null);
+    }
+
+    /**
+     *
+     */
+    private DefaultAction(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return StringUtils.defaultString(value, name());
+    }
+}
